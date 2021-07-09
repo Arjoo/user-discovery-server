3a61bd634fb1106c817b0c330ff5926b90153d90
Arjoo <2009arjoo@gmail.com>
2021-07-10 01:53:20 +0530

    Initial commit

---
+++ src/test/java/com/arjoo/microservices/UserDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjoo.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
